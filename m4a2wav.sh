c81c0bda7e00d63a2fe05b1aa80eb058f7260fa0
panxin <panxin@jumpy.ai>
2024-06-17 16:13:16 +0800

    add m4a to wav

--- m4a2wav.sh
+++ m4a2wav.sh
@@ -6,6 +6,5 @@ wavpath=$1
 filename=$2
 
 # convert from 48k 16bit mono to 16k 16 bit mono
-ffmpeg -i ${wavpath}/${filename}.mp3 -ac 1 -f wav -acodec pcm_s16le ${wavpath}/${filename}.wav
-
+ffmpeg -i ${wavpath}/${filename}.m4a -ac 1 -f wav -acodec pcm_s16le ${wavpath}/${filename}.wav
 # ffmpeg -f s16le -ac 1 -ar 48k -i ${wavpath}/${filename}.wav -ar 16k -ac 1 ${wavpath}/after/${filename}.wav
--- run.sh
+++ run.sh
@@ -11,6 +11,7 @@ for filename in $(ls -l in/ | awk '{print $9}'); do
     # raw2wav can only be used for sednn and its belong test !!
     #./raw2wav.sh /home/share/xu_to_panxin ./in ${temp[0]}
     ./mp32wav.sh ./in ${temp[0]}
+    # ./m4a2wav.sh ./in ${temp[0]}
 done
 
 echo "Done!"
